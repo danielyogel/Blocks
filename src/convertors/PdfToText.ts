3fcb3dfb43edf8d3a7fc0370bd55f5922f0d5271
Daniel Yogel <danielyogel@gmail.com>
2022-04-26 14:29:30 +0300

    feat: pdfToText

---
+++ src/convertors/PdfToText.ts
@@ -0,0 +1,7 @@
+import pdfParse from 'pdf-parse';
+import { NodeValue } from '../SemanticBlocks';
+import { assertNever } from '../utils';
+
+export async function PdfToText(pdf: Buffer): Promise<string> {
+  return (await pdfParse(pdf)).text;
+}
