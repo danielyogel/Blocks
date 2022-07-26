e02eee54451c05ab983958b1dd52d88614d45eee
Daniel Yogel <danielyogel@gmail.com>
2022-07-26 21:48:27 +0300

    fix(typo) + version 0.1.1

--- src/blocks/BODY_SIMPLE.tsx
+++ src/blocks/BODY_SIMPLE.tsx
@@ -6,7 +6,7 @@ const parse = (text: string) => `<h3>${text}<h3/>`;
 type HTML = string;
 
 export const BODY_SIMPLE: Block<HTML> = {
-  Icon: () => <div>BODY</div>,
+  Icon: () => <div>Body</div>,
   initialValue: parse('This is your body'),
   _fromString: parse,
   _toString(html) {
@@ -18,7 +18,7 @@ export const BODY_SIMPLE: Block<HTML> = {
     return (
       <div>
         <div className='font-bold text-xs mb-1' style={{ fontSize: '10px' }}>
-          BODY
+          Body
         </div>
         <div>{RichText(['Bold', 'Italic', 'Strike', 'Underline', 'redo', 'undo', 'Highlight'])(params)}</div>
       </div>
